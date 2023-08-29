c758094b14e995250f39d9bab7109bdca34b9167
hyeri <hrpark9504@gmail.com>
2023-08-29 20:05:44 +0900

    데이터 목록조회

--- test/src/main/java/com/example/test/repository/ArticleRepository.java
+++ test/src/main/java/com/example/test/repository/ArticleRepository.java
@@ -3,7 +3,13 @@ package com.example.test.repository;
 import com.example.test.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article,Long> {  //관리대상,id의 타입
     //부모의 메서드를 그대로 가져와서 사용할 것임
     //JPA가 알아서 구현체 클래스를 생성해줄 것임
+
+
+    @Override
+    ArrayList<Article> findAll();  //오버라이드 해줌 List타입으로 반환할 수 있도록
 }
