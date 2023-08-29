930a0829081044d09a1e60402312942816061f26
hyeri <hrpark9504@gmail.com>
2023-08-29 18:53:28 +0900

    롬복과 리팩터링

--- test/src/main/java/com/example/test/controller/articleController.java
+++ test/src/main/java/com/example/test/controller/articleController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -30,6 +31,17 @@ public class articleController {
         Article saved = articleRepository.save(article);  //저장메서드
         log.info(saved.toString());
         return "";
+    }
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id,Model model){ //파라미터로 받는 값
+        log.info("id="+id);
+        //1. id로 데이터를 가져옴
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //2. 가져온 데이터를 모델에 등록
+        model.addAttribute("article",articleEntity);
+        //3.  보여줄 페이지를 설정
+
+        return "articles/show";
     }
 
 }
